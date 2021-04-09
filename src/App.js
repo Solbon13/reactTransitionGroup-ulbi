018e3d14e25ad31e4cc219bfe72ca2478e51cdb2
Solbon13 <Solbon13@yandex.ru>
2021-04-09 20:36:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Transition } from 'react-transition-group';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loaderVisible, setLoaderVisible] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => setLoaderVisible(true), 1000)
+    setTimeout(() => setLoaderVisible(false), 5000)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? 'hide' : 'show'}</button>
+      <div className='wrap'>
+        <Transition
+          in={loaderVisible}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+          onExit={() => console.log('onExit')}
         >
-          Learn React
-        </a>
-      </header>
+          {state => <div className={`circle ${state}`} />}
+        </Transition>
+      </div>
     </div>
   );
 }
